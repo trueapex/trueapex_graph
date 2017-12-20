9425648e802a1e688eeeb603a682be5d6e6909da
Romualdo Dasig <das@trueapex.com>
2017-12-20 17:22:34 +0800

    Update test script.

--- src/Graph.php
+++ src/Graph.php
@@ -16,11 +16,49 @@ class Graph {
    *
    * @see SimpletestUnitTestExampleTestCase
    */
-  public function render($date_string) {
-    if (empty($date_string) || $date_string == '0000-00-00' || $date_string == '0000-00-00 00:00:00') {
-      return TRUE;
+  public function render($entity, $metrics = array()) {
+    $entities = array(
+      'award_submission',
+      'publication',
+      'achievement',
+      'profile2',
+    );
+    if (!in_array($entity, $entities)) {
+      drupal_set_message('Invalid entity!', 'error');
+      return FALSE;
     }
-    return FALSE;
+
+    // Get Google Charts info.
+    $charts_info = charts_info();
+
+    // Build chart.
+    $chart = array(
+      '#type' => 'chart',
+      '#chart_type' => 'column',
+      '#title' => '',
+    );
+
+    // Get data.
+    $chart += $this->getData($entity, $metrics);
+
+    $chart['#chart_library'] = 'google';
+    $chart['#width'] = 900;
+    $chart['#height'] = 300;
+    
+    return $chart;
+  }
+
+  /**
+   * Get data for chart to display.
+   * 
+   * @param $entity
+   * @param $metrics
+   * @return array
+   */
+  public function getData($entity, $metrics) {
+    $result = array();
+    
+    return $result;
   }
   
 }
